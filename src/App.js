8a283912c5318851848b38889c106e6163f54b38
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2022-11-28 23:27:06 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="w-screen h-auto flex flex-col bg-black text-white">
+      <Header />
+    </div>
+  );
+};
+export default App;
