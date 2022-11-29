9a1396f7ae5308c1f45a5779a3bc1ebecd231b42
Vivian Okpala <okpalavivian86@gmail.com>
2022-11-29 17:49:29 +0000

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col bg-black text-white">
+    <div className="w-screen h-auto flex flex-col">
       <Header />
     </div>
   );
